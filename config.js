7e312c3a90a00f5b2fa314e88d308f1368bb6f16
Álvaro Cotes <alvarocotes.jc@gmail.com>
2026-03-19 13:22:04 -0500

    Update API_BASE URL in config.js to point to a new production backend

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ const CONFIG = {
   // API_BASE: "https://backend-king-perfum.onrender.com",
   // API_BASE: "http://localhost:3000",
   API_BASE:
-    "https://backend-kingperfum-ebi0nfgxl-alvarocotesjc-9801s-projects.vercel.app/",
+    "https://backend-kingperfum-8r92e2nif-alvarocotesjc-9801s-projects.vercel.app/",
 };
